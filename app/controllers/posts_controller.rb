f64917f7f902624d506e2213b956e6c7fb66906a
MajedBojan <bojanmajed@gmail.com>
2018-09-28 16:03:40 +0300

    fix intendent line

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,58 +2,52 @@ class PostsController < BaseController
 #layout false
 #before_action  except: [:index, :show]
 # before_action :confirm_logged_in, except: [:index, :show]
-
-def index
-  @posts = Post.all.order('created_at DESC')
-end
-
-
-def new
-	@post = Post.new
-end
+  def index
+    @posts = Post.all.order('created_at DESC')
+  end
 
 
-def create
-	@post = Post.new(post_params)
-		if @post.save
-		redirect_to @post
-		else
-		render 'new'
-		end
-
-	end
-
-def show
-@post = Post.find(params[:id])
-
-	end
-
-def edit
-	@post = Post.find(params[:id])
-end
+  def new
+    @post = Post.new
+  end
 
 
-def update
-	@post = Post.find(params[:id])
-		if @post.update(post_params)
-		redirect_to @post
-		else
-	render 'edit'
-	end
-	end
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
 
-def destroy
+  def show
+    @post = Post.find(params[:id])
+  end
 
-@post = Post.find(params[:id])
-@post.destroy
-#redirect_to posts_path
-redirect_to root_path
-end
-private
+  def edit
+    @post = Post.find(params[:id])
+  end
 
-def post_params
-params.require(:post).permit(:title, :body, :image)
-end
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
 
+  def destroy
 
+    @post = Post.find(params[:id])
+    @post.destroy
+    #redirect_to posts_path
+    redirect_to root_path
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body, :image)
+  end
 end
